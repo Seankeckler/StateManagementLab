f667dad113ea2a0f139ee21b71aad08422c31c4f
Sean Keckler <seankeckler@gmail.com>
2018-11-07 15:51:16 -0500

    First Commit

---
+++ StateManagementLab/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using StateManagementLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateManagementLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Result(Registration r)
+        {
+
+            if (Session["one"] == null)
+            {
+                Session["one"] = "active";
+                Session["Name"] = r.userName;
+                Session["Email"] = r.email;
+                Session["Age"] = r.age;
+            }
+           
+           
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            ViewBag.name = Session["Name"];
+            Session["one"] = null;
+
+            return View();
+        }
+    }
+}
---
+++ StateManagementLab/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StateManagementLab.Models
+{
+    public class Registration
+    {
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+    }
+}
